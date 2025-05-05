cf2fb276ea8979d3c5db41a7a64c27f6833bec9e
Jarryd Deane <jarryd@synthesis.co.za>
2025-05-05 12:07:19 +0200

    Add lifecycle and external config tests

---
+++ K6Testing/Scripts/adhoc-test.js
@@ -0,0 +1,20 @@
+﻿import http from 'k6/http';
+import { sleep } from 'k6';
+import exec from 'k6/execution';
+
+// Load the test configuration from a JSON file
+// Hosts key - Overrides the DNS resolution for specific hosts. For example, when calling test.k6.io, the url will go to 1.2.3.4
+// userAgent key - Overrides the default k6 user agent that is set
+// noConnectionReuse key - Specifies whether K6 should disable keep alive connections
+// discardResponseBodies key - Discard http responses when not needed (recommended). Reduces memory usage and provides more accurate results. 
+// You can still return values for http requests that require it
+const testConfig = JSON.parse(open('./config-test.json'));
+
+export const options = testConfig;
+
+export default function() {
+    http.get('https://localhost:7140/string/reverse?input=hello');
+    console.log(exec.test.options.scenarios.default.vus);
+    sleep(1);
+}
+
---
+++ K6Testing/Scripts/lifecycle-test.js
@@ -0,0 +1,35 @@
+﻿// 1. init code
+// All code that is outside of a lifecycle function is code in the init context
+import http from 'k6/http';
+import { Trend } from 'k6/metrics';
+
+console.log('Init code');
+
+export const options = {
+    vus: 1,
+    iterations: 5
+}
+
+export function setup() {
+    // 2. setup code
+    console.log('Setup code');
+    return { v: 1 };
+}
+
+export default function (data) {
+    // 3. VU code
+    console.log('VU code: ' + data.v);
+}
+
+export function teardown(data) {
+    // 4. teardown code
+    // This function is not called if setup throws an exception. Handle errors in setup in order for teardown to be called
+    console.log('Teardown code: ' + data.v);
+}
+
+export function handleSummary(data) {
+    // 5. handleSummary code
+    // This function is called after the test has finished and is used to generate a summary report
+    console.log('Summary code');
+    console.log(data);
+}

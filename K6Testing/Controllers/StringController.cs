f776232028b719582ee902eba2855e126beba71f
Jarryd Deane <jarryd@synthesis.co.za>
2025-05-05 17:04:18 +0200

    Set cookies and validate them when server requires certain cookies

--- K6Testing/Controllers/StringController.cs
+++ K6Testing/Controllers/StringController.cs
@@ -16,6 +16,25 @@ public class StringController : ControllerBase
             reverse.Append(input[i]);
         }
         Thread.Sleep(500);
+
+        if (Request.Headers.Cookie.Count > 0)
+        {
+
+        }
+
+        for (int i = 0; i < Request.Headers.Cookie.Count; i++)
+        {
+            var cookies = Request.Headers.Cookie[i].Split(';');
+
+            for (int j = 0; j < cookies.Length; j++)
+            {
+                var cookie = cookies[j].Trim().Split('=');
+                var cookieKey = cookie[0];
+                var cookieValue = cookie[1].Replace("\"", "");
+                Response.Cookies.Append(cookieKey, cookieValue);
+            }
+        }        
+
         return reverse.ToString();
     }
 }
--- K6Testing/Scripts/enable-es6-features-webpack/src/bundle-test.js
+++ K6Testing/Scripts/enable-es6-features-webpack/src/bundle-test.js
@@ -1,9 +1,47 @@
 ﻿import http from 'k6/http';
-import { sleep } from 'k6';
+import { sleep, check } from 'k6';
 import { SomeService } from './some.service.js';
 
-export default function() {
-    http.get('https://localhost:7140/string/reverse?input=hello');
+export default function () {
+    // Set global cookies for a particular url
+    const jar = http.cookieJar();
+    jar.set('https://localhost:7140/string/reverse', 'my_cookie', 'hello world');
+
+    // Override cookie jar my_cookie and add custom cookie (once_off_cookie) for single request
+    const cookies = {
+        my_cookie: {
+            value: 'hello world 2',
+            replace: true
+        },
+        once_off_cookie: {
+            value: 'once off cookie',
+            replace: false
+        }
+    };
+
+    const res = http.get('https://localhost:7140/string/reverse?input=hello', {
+        // Sets cookies for a single request
+        cookies
+    });
+
     let someService = new SomeService();
+
+    // Get cookies from jar for particular url
+    const cookiesJar = jar.cookiesForURL('https://localhost:7140/string/reverse');
+
+    // Validate jar cookies
+    check(cookiesJar, {
+        "has cookie 'my_cookie'": (cookiesJar) => cookiesJar.my_cookie.length > 0,
+        'my_cookie has correct value': (cookiesJar) => cookiesJar.my_cookie[0] === 'hello world'
+    });
+
+    // Validate response cookies
+    check(res, {
+        "has cookie 'my_cookie'": (r) => r.cookies.my_cookie.length > 0,
+        'my_cookie has correct value': (r) => r.cookies.my_cookie[0].value === 'hello%20world%202',
+        "has cookie 'once_off_cookie'": (r) => r.cookies.once_off_cookie.length > 0,
+        'once_off_cookie has correct value': (r) => r.cookies.once_off_cookie[0].value === 'once%20off%20cookie',
+    });
+
     sleep(1);
 }

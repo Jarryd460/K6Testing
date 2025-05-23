c2254ca7cff2edaa1f467c91de4c9ede377b6df6
Jarryd Deane <jarryd@synthesis.co.za>
2025-05-23 16:52:01 +0200

    Add scenarios that execute different methods

---
+++ K6Testing/Scripts/scenarios/custom-exec-scenario-test.js
@@ -0,0 +1,27 @@
+﻿import http from 'k6/http';
+
+export const options = {
+    scenarios: {
+        // Iterates 20 times among 5 users - iterations are not guaranteed to be shared equally among vus
+        method1scenario: {
+            executor: 'shared-iterations', // Other options - per-vu-iterations, constant-vus, ramping-vus, constant-arrival-rate, ramping-arrival-rate
+            vus: 5,
+            iterations: 200,
+            exec: 'method1'
+        },
+        method2scenario: {
+            executor: 'constant-vus', // Other options - per-vu-iterations, constant-vus, ramping-vus, constant-arrival-rate, ramping-arrival-rate
+            vus: 2,
+            duration: '10s',
+            exec: 'method2'
+        }
+    }
+}
+
+export function method1() {
+    http.get('https://localhost:7140/string/reverse?input=hello');
+}
+
+export function method2() {
+    http.get('https://localhost:7140/string/reverse?input=hello');
+}
